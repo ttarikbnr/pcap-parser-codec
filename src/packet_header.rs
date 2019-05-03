aba5f2be8c3ca93dfd07ecac0dc92f81ebed0103
Tarik Binar <ttarikbnr@gmail.com>
2019-05-04 00:05:31 +0300

    packetheader from raw

--- src/packet_header.rs
+++ src/packet_header.rs
@@ -1,17 +1,47 @@
 use nom::{be_u32, le_u32};
 
 pub struct PacketHeader {
-    timestamp: u64,
-    packet_length: u32
+    timestamp_ns: u64,
+    packet_length: usize 
+}
+
+impl PacketHeader {
+    pub fn from_raw(raw_header: &PacketHeaderRaw, is_ns: bool) -> Self {
+        let timestamp_ns = raw_header.get_timestamp_as_ns(is_ns);
+        let packet_length = raw_header.get_packet_length();
+
+        Self {
+            timestamp_ns,
+            packet_length
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
-struct PacketHeaderRaw {
+pub struct PacketHeaderRaw {
     ts_sec      : u32,
     ts_usec     : u32,
     incl_len    : u32,
     orig_len    : u32,
 }
+
+impl PacketHeaderRaw {
+    fn get_timestamp_as_ns(&self, is_ns: bool) -> u64 {
+        let sec_as_ns = self.ts_sec  as u64 * 1_000_000_000;
+        let sub_sec_as_ns = if is_ns {
+            self.ts_usec as u64
+        } else {
+            self.ts_usec as u64 * 1000
+        };
+
+        sec_as_ns + sub_sec_as_ns
+    }
+
+    fn get_packet_length(&self) -> usize {
+        self.orig_len as usize
+    }
+}
+
 
 
 named!(parse_packet_header<PacketHeaderRaw>,
